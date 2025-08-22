bc95d5647087c9090ac1e1a6b013a7ffc1798fb7
Connor <oneicon000@gmail.com>
2025-08-22 08:45:10 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("app-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/script.js",
+        "/data.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
