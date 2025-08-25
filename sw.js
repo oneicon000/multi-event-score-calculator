7e230cbdcbd612d0d511d7d033262ead42d5c012
Connor <oneicon000@gmail.com>
2025-08-25 13:50:53 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,12 +2,19 @@ self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("app-cache").then((cache) => {
       return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/script.js",
-        "/data.js"
-      ]);
+        "./",
+        "./index.html",
+        "./style.css",
+        "./script.js",
+        "./data.js",
+        "./manifest.json",
+        "./maskable_icon_192x192.png",
+        "./maskable_icon_512x512.png",
+        "./icon-192.png",
+        "./icon-512.png",
+      ]).catch((err) => {
+        console.warn("SW cache failed:", err);
+      });
     })
   );
 });
