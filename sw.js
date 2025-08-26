725e882b8135280e3142cd58849e363780b23aef
Connor <oneicon000@gmail.com>
2025-08-25 21:53:49 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,35 @@
+// Change this version whenever you deploy new updates
+const CACHE_NAME = "app-cache-v2";
+
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open("app-cache").then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./script.js",
-        "./data.js",
-        "./manifest.json",
-        "./maskable_icon_192x192.png",
-        "./maskable_icon_512x512.png",
-        "./icon-192.png",
-        "./icon-512.png",
-      ]).catch((err) => {
-        console.warn("SW cache failed:", err);
-      });
+        "/",
+        "/index.html",
+        "/style.css",
+        "/script.js",
+        "/data.js", 
+        "/manifest.json", 
+        "/maskable_icon_192x192.png",
+        "/maskable_icon_512x512.png",
+        "/icon-192.png",
+        "/icon-512.png",
+      ]);
     })
   );
 });
 
+// Remove old caches when activating new service worker
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+// Fetch from cache, fall back to network if not cached
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
