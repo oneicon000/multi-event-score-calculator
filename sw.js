51bd5510aafe14c8ba695b05fc796d0c1ec106d3
Connor <oneicon000@gmail.com>
2025-10-16 10:22:05 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version whenever you deploy new updates
-const CACHE_NAME = "app-cache-v5";
+const CACHE_NAME = "app-cache-v6";
 
 self.addEventListener("install", (e) => {
   self.skipWaiting(); // activate immediately
@@ -66,6 +66,7 @@ self.addEventListener("fetch", (e) => {
     })
   );
 });
+
 
 
 
